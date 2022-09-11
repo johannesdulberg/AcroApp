4018a9e0b246bf36ea35d21d7a32520afeeb1c05
Johannes <johannesduelberg@web.de>
2022-09-11 22:19:58 +0200

    pics and saves

--- App.js
+++ App.js
@@ -1,24 +1,102 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text ,Image,TextInput} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Moves from './screens/moves';
 import MoveDetails from './screens/moveDetails';
-
-
+import {useState, useEffect} from 'react'
+import * as ImagePicker from "expo-image-picker"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 function HomeScreen({ navigation }) {
+
+  //ASYNC STUFF
+  const saveImage =async(imageUri)=>{
+    try{
+      if (imageUri!==null){
+        AsyncStorage.setItem("Image",imageUri)
+        console.log("SAVED IMAGE",imageUri)
+      }
+      
+    }catch(error){
+      console.log(error)
+    }
+  }
+const getImage=async()=>{
+  try{
+    const name = await AsyncStorage.getItem("Image")
+    setImage(name)
+
+
+  }catch(error){
+    console.log(error)
+  }
+}
+
+useEffect(()=>{
+  getImage()
+},[]
+)
+
+const deleteName=()=>{
+  AsyncStorage.removeItem("appData")
+  setTextFromUser("")
+}
+  //ASYNC STUFF END
+
+
+  //IMAGE PICKER
+
+  const [hasGalleryPermission, setHasGalleryPermission] =useState(null)
+  const [image,setImage]=useState(null)
+
+
+  useEffect(() => {
+    (async() => {
+      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync()
+      setHasGalleryPermission(galleryStatus.status==="granted")
+
+    })()
+  },[])
+
+
+
+
+
+  const pickImage =async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect:[4,3],
+      quality:1,
+    })
+
+    if (!result.cancelled){
+      setImage(result.uri)
+      saveImage(result.uri)
+      console.log("RESULT",result.uri)
+
+    }
+    
+
+  }
+  if (hasGalleryPermission === false){
+    return <Text>No access to storage</Text>
+  }
+
+    //IMAGE PICKER END
   return (
 
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen ficken</Text>
+      <View>
+        <Button title="pick Image" onPress={() => pickImage()}/>
+        <Image source={{uri:image}} style={{flex:1/2}}/>
+      </View>
       <Button
         title="Moves"
         onPress={() => navigation.navigate('Moves')}
       />
     </View>
-    
-
   );
 }
 
